7074111eb424f01dccca63bc38c175a44472ef74
Duong Le Dinh Duy <dinhduydt02@gmail.com>
2023-09-10 22:52:41 +0700

    Edit bikeshare file

--- bikeshare.py
+++ bikeshare.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 #Create dictionary of city data - 1st time for practicing
-
+#Create dictionary of city data - 2nd time for practicing
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
