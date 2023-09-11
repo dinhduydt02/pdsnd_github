d41012dec05359f29b2ae39ccf0903a3334a102f
Duong Le Dinh Duy <dinhduydt02@gmail.com>
2023-09-11 15:53:48 +0700

    master: give more detail comments

--- bikeshare.py
+++ bikeshare.py
@@ -5,6 +5,7 @@ import numpy as np
 #Create dictionary of city data - 1st time for practicing
 #Create dictionary of city data - 2nd time for practicing
 #Create dictionary of city data - 3rd time for practicing
+#Give more information details
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
